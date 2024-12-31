d6a6ba721757643dd4f93b3fa737d344fa9a26eb
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2024-12-31 14:01:58 -0600

    Add Prettier configuration for Astro project

---
+++ .prettierrc.mjs
@@ -0,0 +1,22 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 4,
+  trailingComma: "es5",
+  singleQuote: false,
+  jsxSingleQuote: false,
+  quoteProps: "consistent",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
