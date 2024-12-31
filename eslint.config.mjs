7a8776e209b7a6aa7d8e414194d6899eaadf3158
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2024-12-31 14:11:00 -0600

    Apply prettier formatting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginAstro from "eslint-plugin-astro";
 
 /** @type {import("eslint").ESLint.ConfigData} */
 export default [
     ...eslintPluginAstro.configs.recommended,
     {
-        rules: {
-
-        }
-    }
+        rules: {},
+    },
 ];
