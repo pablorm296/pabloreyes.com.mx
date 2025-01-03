44bca68784a3df567f1cdec9583747724122f39b
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2025-01-03 14:24:52 -0600

    Add sitemap integration and update site URL in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://pabloreyes.com.mx",
     markdown: {
         shikiConfig: {
-            theme: "material-theme"
-        }
-    }
+            theme: "material-theme",
+        },
+    },
+    integrations: [sitemap()],
 });
